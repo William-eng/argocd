24f06ecd03bb7ec77e8ae460f100ff147745f4b4
William-eng <williamtijesuni@gmail.com>
2025-02-07 10:57:24 +0100

    first commit

---
+++ argocd-install/argo.tf
@@ -0,0 +1,26 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "= 2.17.0"
+    }
+  }
+}
+
+
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "3.35.4"
+  values           = [file("install.yaml")]
+}
